ac0fb155dd679bd6cce898e86c8aa352841e8326
Danush Ramanan <dan.ramanan@gmail.com>
2023-11-14 10:23:31 -0700

    create board module

---
+++ src/board.rs
@@ -0,0 +1,31 @@
+struct bitboards {
+	white_pawns: u64,
+	white_rooks: u64,
+	white_knights: u64,
+	white_bishops: u64,
+	white_queens: u64,
+	white_king: u64,
+	white_all: u64,
+	black_pawns: u64,
+	black_rooks: u64,
+	black_knights: u64,
+	black_bishops: u64,
+	black_queens: u64,
+	black_king: u64,
+	black_all: u64,
+}
+
+fn load_fen(fen: String) {
+				
+}
+
+fn update_bitboards() {
+
+}
+
+fn make_move() {
+
+}
+fn undo_move() {
+
+}
