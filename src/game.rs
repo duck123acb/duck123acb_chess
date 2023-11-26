66c55d1ea711c4cc30b77ecccbbf2edc12b81e75
Danush Ramanan <dan.ramanan@gmail.com>
2023-11-26 07:48:23 -0700

    split fen into array

--- src/game.rs
+++ src/game.rs
@@ -14,25 +14,29 @@ struct Bitboards {
 	black_king: u64,
 	black_all: u64,
 }
+struct CastlingRights {
+	white_kingside: bool,
+	white_queenside: bool,
+	black_kingside: bool,
+	black_queenside: bool,
+}
 
 pub struct Info {
 	bitboards: Bitboards,
 	whites_turn: bool,
-	white_can_castle_kingside: bool,
-	white_can_castle_queenside: bool,
-	black_can_castle_kingside: bool,
-	black_can_castle_queenside: bool,
-	en_passent_coordinate: &str,
-	moves_since_capture_or_pawn_push: u32,
-	turns: u32
+	castling_rights: CastlingRights,
+	en_passent_coordinate: String, // to know if there is en passent
+	moves_since_capture_or_pawn_push: u32, // for the 50 move rule
+	turns: u32 // every white and black move
+}
+
+fn update_bitboards() {
+
 }
 
 pub fn load_fen(fen: &str) {
-	println!("{}", fen);
-}
-
-pub fn update_bitboards() {
-
+	let fen_fields: Vec<&str> = fen.split(' ').collect(); // 0 = piece_arrangement, 1 = who's turn, 2 = castling rights, 3 = any en_passent squares, 4 = moves countine toward the 50 move rule, 5 = how many turns has happened in a game (a turn is white goes, and black goes)
+	println!("{}, {}, {}, {}, {}, {}", fen_fields[0], fen_fields[1], fen_fields[2], fen_fields[3], fen_fields[4], fen_fields[5]);
 }
 
 pub fn make_move(coordinate_one: &str, coordinate_two: &str) {
